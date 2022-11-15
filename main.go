b2f7e1a588c235ca531332fb7d600f87e375c31a
Stepan Rodionov <Stepan.Rodionov@x5.ru>
2022-11-15 11:38:46 +0300

    send message

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 )
@@ -14,10 +15,31 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/v1/message", sendHandler)
 	err := http.ListenAndServe(":"+port, mux)
 	if err != nil {
 		return
 	}
+}
+
+func sendHandler(w http.ResponseWriter, r *http.Request) {
+	var method = r.Method
+	var err error
+	var res string
+	switch method {
+	case http.MethodGet:
+		res, err = "Get messages", nil
+	case http.MethodPost:
+		res, err = "Produce message", nil
+	default:
+		res, err = fmt.Sprintf("method %s not allowed", method), nil
+	}
+	if err != nil {
+		w.Write([]byte("Непредвиденная ошибка!"))
+		return
+	}
+
+	w.Write([]byte(res))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
